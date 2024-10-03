878eebf6cb6d9a4d997e65315a9df0b220515822
ahmad aiman <ahmadaimanalias@gmail.com>
2024-10-03 19:04:21 +0800

    feat: ready for production

--- warehouse_website/settings.py
+++ warehouse_website/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%k5vwlacx)o#hh7q6avh!+jawg^cty964a-48fr+c!b@_!zu=*'
+SECRET_KEY = 'pwyw*)hg-_+sy=eny)6oo5jbd$pu1jy8=36+x%wm8cji5l3p*6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -153,13 +153,18 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5173"
-# ]
+CORS_ALLOWED_ORIGINS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
 CORS_ALLOW_CREDENTIALS = False
 CORS_ALLOW_HEADERS = (
     'content-type',
     'Authorization',
 )
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = True
+SECURE_HSTS_PRELOAD = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
