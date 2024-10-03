204124d1cbbe577d26deb0dc807bdff35f00545d
ahmad aiman <ahmadaimanalias@gmail.com>
2024-10-03 20:33:44 +0800

    fix bugs

--- warehouse_website/settings.py
+++ warehouse_website/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -155,7 +155,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
+CORS_ALLOWED_ORIGINS = os.getenv('DJANGO_ALLOWED_ORIGINS', '').split(',')
 CORS_ALLOW_CREDENTIALS = False
 CORS_ALLOW_HEADERS = (
     'content-type',
