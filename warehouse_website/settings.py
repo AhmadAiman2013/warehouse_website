78521ed868eb9bff01289d12146eec1156dd2cfa
ahmad aiman <ahmadaimanalias@gmail.com>
2024-10-03 21:15:41 +0800

    fix:bug

--- warehouse_website/settings.py
+++ warehouse_website/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.getenv('DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = [
+    "https://apiwarehouse.ahmdaimn.xyz"
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
